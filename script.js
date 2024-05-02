c83f18d929748d2f51be80abe5d3042717833e4f
ambarcedeno2 <137855484+ambarcedeno2@users.noreply.github.com>
2024-05-02 15:41:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Espera a que el DOM se haya cargado completamente antes de ejecutar el código
 document.addEventListener("DOMContentLoaded", function() {
+  // Obtiene el formulario de registro por su ID
   var registerForm = document.getElementById("registerForm");
 
+  // Agrega un evento de escucha para el evento "submit" en el formulario
   registerForm.addEventListener("submit", function(event) {
+    // Obtiene los valores de los campos del formulario
     var nombre = document.getElementById("nombre").value;
     var apellido = document.getElementById("apellido").value;
     var email = document.getElementById("email").value;
@@ -11,19 +15,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Validación de campos vacíos
     if (nombre === "" || apellido === "" || email === "" || usuario === "" || password === "" || confirmPassword === "") {
-      event.preventDefault();
-      alert("Por favor, completa todos los campos.");
-      return;
+      event.preventDefault(); // Evita que se envíe el formulario
+      alert("Por favor, completa todos los campos."); // Muestra un mensaje de alerta al usuario
+      return; // Detiene la ejecución del código
     }
 
     // Validación de coincidencia de contraseña
     if (password !== confirmPassword) {
-      event.preventDefault();
-      alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
-      return;
+      event.preventDefault(); // Evita que se envíe el formulario
+      alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo."); // Muestra un mensaje de alerta al usuario
+      return; // Detiene la ejecución del código
     }
 
-    // Si la validación pasa, mostrar mensaje de éxito
+    // Si la validación pasa, muestra un mensaje de éxito al usuario
     alert("Usuario registrado con éxito.");
   });
 });
